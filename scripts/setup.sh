6d87da75e691f2ea205347ef2057596c21db4b37
karuru6225 <e2hjhi@gmail.com>
2014-07-01 11:56:35 +0900

    setup.shをもっとシンプルにしたい。

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -4,68 +4,26 @@
 #ワンライナー
 #curl -kL https://raw.github.com/kiyohiro-kano/chef-repo-skeleton/master/scripts/setup.sh | bash && git clone https://github.com/kiyohiro-kano/chef-repo-skeleton.git && cd chef-repo && ./scripts/prepare.sh && ./scripts/run_chef-solo.sh
 
-set -e
+set -ex
 
 if [ -f /etc/redhat-release ]; then
-	UNAME=`cat /etc/redhat-release`
-	if [[ "${UNAME}" =~ .*CentOS.* ]];then
-		OS="centos"
-#		if [ "${REPO_URL}" != "" ];then
-#			RURL=${REPO_URL%/}
-#			RURL=${RURL//\//\\\/}
-#			mv /etc/yum.repos.d/CentOS-Base.repo{,.bak}
-#			mv /etc/yum.repos.d/epel.repo{,.bak}
-#			sed -e "s/^mirror.*\(updates\|os\)$/#\0\nbaseurl=${RURL}\/\1\//" /etc/yum.repos.d/CentOS-Base.repo.bak > /etc/yum.repos.d/CentOS-Base.repo
-#			sed -e "s/^mirror.*\(epel\).*$/#\0\nbaseurl=${RURL}\/\1\//" /etc/yum.repos.d/epel.repo.bak > /etc/yum.repos.d/epel.repo
-#		fi
-	fi
+	yum -y install git gcc gcc-c++ automake autoconf make openssl-devel.x86_64
 elif [ -f /etc/debian_version ]; then
-	OS="debian"
+	apt-get -y install git build-essential libssl-dev libreadline5-dev
 else
 	echo "unsupported OS"
 	exit 1
 fi
 
-set -ex
-
-if [ "${OS}" == "centos" ]; then
-	yum -y install git gcc gcc-c++ automake autoconf make openssl-devel.x86_64
-elif [ "${OS}" == "debian" ]; then
-	apt-get -y install git build-essential libssl-dev libreadline5-dev
+if ! type chef-solo >/dev/null 2>&1; then
+	curl -L https://www.opscode.com/chef/install.sh | bash  
 fi
 
-cd /usr/local/
-rm -rf rbenv
-git clone git://github.com/sstephenson/rbenv.git rbenv
+cat <<'EOF' > Gemfile
+source 'https://rubygems.org'
+gem 'berkshelf'
+EOF
 
-cat > /etc/profile.d/rbenv.sh << EOT
-#!/bin/bash
+/opt/chef/embedded/bin/bundle install --path vendor/bundle
 
-export RBENV_ROOT=/usr/local/rbenv
-export PATH="\$RBENV_ROOT/bin:\$PATH"
-eval "\$(rbenv init -)"
-EOT
-
-chmod u+x /etc/profile.d/rbenv.sh
-. /etc/profile.d/rbenv.sh
-
-if [ "`grep rbadmin /etc/group`" == "" ]; then
-	/usr/sbin/groupadd rbadmin
-fi
-chgrp -R rbadmin rbenv
-chmod -R g+rwxXs rbenv
-
-mkdir /usr/local/rbenv/plugins
-cd /usr/local/rbenv/plugins
-git clone git://github.com/sstephenson/ruby-build.git
-chgrp -R rbadmin ruby-build
-chmod -R g+rwxs ruby-build
-
-git clone https://github.com/ianheggie/rbenv-binstubs.git 
-chgrp -R rbadmin rbenv-binstubs
-chmod -R g+rwxs rbenv-binstubs
-
-rbenv install 2.0.0-p353
-rbenv global 2.0.0-p353
-gem install bundler --no-rdoc --no-ri
-
+#/opt/chef/embedded/bin/gem
