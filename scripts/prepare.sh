b9323e05642c151a4914edbd4704dcda03033175
karuru6225 <e2hjhi@gmail.com>
2014-07-19 01:49:51 +0900

    Update prepare.sh

--- scripts/prepare.sh
+++ scripts/prepare.sh
@@ -7,8 +7,8 @@ cd `dirname $0`; cd ../
 if ! type chef-solo >/dev/null 2>&1; then
 	curl -L https://www.opscode.com/chef/install.sh | bash	
 fi
-BASE="/opt/chef/embedded/bin"
-PATH=${PWD}/.bundle/bin:${BASE}:${PATH}
+#BASE="/opt/chef/embedded/bin"
+#PATH=${PWD}/.bundle/bin:${BASE}:${PATH}
 bundle install --path ./.bundle/gems --binstubs ./.bundle/bin
 berks install --path ./cookbooks
 ./config_solo.sh
