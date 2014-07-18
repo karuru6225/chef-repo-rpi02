c4d2641b76567bd7d1e5a9ad9c801d216d7f8733
karuru6225 <e2hjhi@gmail.com>
2014-07-19 01:50:35 +0900

    Update setup.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -57,7 +57,7 @@ git clone https://github.com/ianheggie/rbenv-binstubs.git
 chgrp -R rbadmin rbenv-binstubs
 chmod -R g+rwxs rbenv-binstubs
 
-rbenv install 2.0.0-p353
-rbenv global 2.0.0-p353
-gem install bundler --no-rdoc --no-ri
+#rbenv install 2.0.0-p353
+#rbenv global 2.0.0-p353
+#gem install bundler --no-rdoc --no-ri
 
