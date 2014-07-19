0089f286909e422f124474314f00bb4c64f1fa96
karuru6225 <e2hjhi@gmail.com>
2014-07-19 16:24:04 +0900

    Update prepare.sh

--- scripts/prepare.sh
+++ scripts/prepare.sh
@@ -8,6 +8,7 @@ if ! type chef-solo >/dev/null 2>&1; then
 	curl -L https://www.opscode.com/chef/install.sh | bash	
 fi
 
+bundle config build.nokogiri --use-system-libraries
 bundle install --path ./.bundle/gems --binstubs ./.bundle/bin
 rbenv rehash
 berks vendor ./cookbooks
