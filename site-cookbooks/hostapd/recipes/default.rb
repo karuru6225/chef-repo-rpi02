73407a75275b9abc1c57c0a56726fee50d2b6c19
karuru6225 <e2hjhi@gmail.com>
2015-11-02 02:18:40 +0900

    とりあえずhostapdをインストールするところまで

---
+++ site-cookbooks/hostapd/recipes/default.rb
@@ -0,0 +1,12 @@
+#
+# Cookbook Name:: hostapd
+# Recipe:: default
+#
+# Copyright 2015, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+package 'hostapd' do
+  action :install
+end
