4562b22855b73093e62b88846e61092f5c7eed67
karuru6225 <e2hjhi@gmail.com>
2015-11-20 19:22:29 +0900

    upnp機能を有効にした。

--- site-cookbooks/iptables/recipes/router.rb
+++ site-cookbooks/iptables/recipes/router.rb
@@ -12,3 +12,20 @@ template '/etc/iptables/iptables' do
   )
   notifies :restart, 'service[iptables]', :delayed
 end
+
+package 'linux-igd' do
+  action :install
+end
+
+template '/etc/default/linux-igd' do
+  source 'linux-igd.erb'
+  owner 'root'
+  group 'root'
+  mode 00644
+  notifies :restart, 'service[linux-igd]', :delayed
+end
+
+service 'linux-igd' do
+  action :enable
+end
+
